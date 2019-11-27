924df73ea648ea69387596986f8090240919257d
Vlad <43403715+Vlad-Harutyunyan@users.noreply.github.com>
2019-11-27 16:09:00 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,64 +1,75 @@
 # #Question 1
 #getting number as string 
-input_number_quest1 = (input('Type number for question 1, please : '))
+input_number_quest1 = int(input('Type number for question 1, please : '))
+start_number = input_number_quest1 
 #creating variable
 num_sum = 0;
 #looping 
-for x in range(len(input_number_quest1)):
-    if int(input_number_quest1[x]) != 0  :
-        num_sum += int(input_number_quest1[x])
+while input_number_quest1 > 0:
+    num_sum+=input_number_quest1%10
+    input_number_quest1 = input_number_quest1//10
     
-print('The sum of digits of', input_number_quest1, 'is' , num_sum , );
+print('The sum of digits of', start_number, 'is' , num_sum , );
 # # --------------------------------------------------------
 
 # #Question 2
-input_number_quest2 = (input('Type number for question 2, please : '))
+input_number_quest2 = int(input('Type number for question 2, please : '))
 #set variable for price
 price = 0;
 
-for x in range(len(input_number_quest2)):
-    if int(input_number_quest2[x])==8 :
-    #if in car numbers we have 8 adding 1000 yuans 
+while input_number_quest2 > 0:
+     # if Given the number plate is biger 10^9 
+    if input_number_quest2 > 10**9 :
+        print(' Given the number plate is big 10^9')
+        break
+    #if in car plate numbers we have 8 adding 1000 yuans 
+    elif input_number_quest2%10 == 8:
         price += 1000
+    input_number_quest2 = input_number_quest2//10
+print('Price :' , price , 'yuans')
 
-
-# if Given the number plate is biger 10^9 
-if int(int(input_number_quest2)) > 10**9 :
-
-    print(' Given the number plate is big 10^9')
-else :
-    print('Price :' , price , 'yuans')
 # # --------------------------------------------------------
 # #Question 3
-input_number_quest3 = (input('Type number for question 3, please : '))
+input_number_quest3 = int(input('Type number for question 3, please : '))
 #creating checking variable 
 check = True
 #looping
-for i in range(len(input_number_quest3)-1) :
+b = []
+while input_number_quest3 > 0 and input_number_quest3 >= 10:
+  b.append(input_number_quest3 % 10)
+  input_number_quest3 = input_number_quest3 // 10
+b = b[::-1] 
+if(input_number_quest3 < 10):
+    for i in range(len(b)-1) :
 
-    if input_number_quest3[i] < input_number_quest3[i + 1] :
-        check = False;
-        break;
-#checking
-if check == True :
-    print('no')
+        if b[i] < b[i + 1] :
+            check = False;
+            break;
+    #checking
+    if check == True :
+        print('no')
+    else:
+        print('yes')
 else:
-    print('yes')    
-
+    print('no')
 # --------------------------------------------------------
 #Question 4
-input_number_quest4 = (input('Type number for question 4, please : '))
+
+input_number_quest4 = int(input('Type number for question 4, please : '))
 #create variable for storing even and add index`s numbers
 even = 0;
 odd = 0;
 #looping
-for x in range(len(input_number_quest4)):
+cnt = 0
+while input_number_quest4 > 0 :
     #if even
-    if x%2==0 :
-        even+= int(input_number_quest4[x])
+    cnt+=1
+    if cnt%2 == 0:
+        even+=input_number_quest4%10
     #if odd
-    else :
-        odd += int(input_number_quest4[x])
+    else:
+        odd+=input_number_quest4%10
+    input_number_quest4=input_number_quest4//10
 #checking
 if even == odd :
     print('Yes , Lucky')
@@ -69,36 +80,41 @@ else :
 #Question 5
 input_number_quest5 = int((input('Type number for question 5, please : ')))
 print(input_number_quest5)
-n_sum = 0;
 while True :
     if input_number_quest5 < 0 :
         break
     while input_number_quest5 >= 10:
         n_sum = 0
         while input_number_quest5>0:
-            d = input_number_quest5 % 10;
-            input_number_quest5 = input_number_quest5 // 10;
-            n_sum += d;
+            d = input_number_quest5 % 10
+            input_number_quest5 = input_number_quest5 // 10
+            n_sum += d
         input_number_quest5=n_sum  
         print(n_sum)
-        break;
-     
+    break;
 # --------------------------------------------------------
 #Question 6
-input_number_quest6 = ((input('Type number for question 6, please : ')))
+input_number_quest6 = int((input('Type number for question 6, please : ')))
 #creating variables for 2 different digits 
-first_digit = int(input_number_quest6[0])
+fe = []
+while input_number_quest6 > 0:
+  fe.append(input_number_quest6 % 10)
+  input_number_quest6 = input_number_quest6 // 10
+fe = fe[::-1] 
+
+first_digit = int(fe[0])
 second_digit = None
 #boolean variable for checking
+
 ch = True
 #first loop finding different second variable and store
-for x in range(len(input_number_quest6)) :
-    if(first_digit != int(input_number_quest6[x])):
-        second_digit = int(input_number_quest6[x])
+for x in range(len(fe)) :
+    if(first_digit != int(fe[x])):
+        second_digit = int(fe[x])
         break;
 #second loop just checking are all elements in number  equal or this two or not
-for x in range(len(input_number_quest6)) :
-    if first_digit != int(input_number_quest6[x]) and second_digit != int(input_number_quest6[x]) :
+for x in range(len(fe)) :
+    if first_digit != int(fe[x]) and second_digit != int(fe[x]) :
         ch=False
         break;
 #here just checking and typing 
